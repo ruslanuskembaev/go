263e0e0845a28f06861a6c82c3546b9d31cb00f3
Руслан Ускембаев <ruslanuskembaev@MacBook-Pro-Ruslan.local>
2025-04-13 21:59:40 +0700

    7/141 Done

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -4,38 +4,26 @@ import "time"
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	parsedDate, err := time.Parse("1/2/2006 15:04:05", date)
-	if err != nil {
-		return time.Time{}
-	}
+	parsedDate, _ := time.Parse("1/2/2006 15:04:05", date)
 	return parsedDate
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	parsedDate, err := time.Parse("January 2, 2006 15:04:05", date)
-	if err != nil {
-		return false
-	}
+	parsedDate, _ := time.Parse("January 2, 2006 15:04:05", date)
 	return parsedDate.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	parsedDate, err := time.Parse("Monday, January 2, 2006 15:04:05", date)
-	if err != nil {
-		return false
-	}
+	parsedDate, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
 	hour := parsedDate.Hour()
 	return hour >= 12 && hour < 18
 }
 
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
-	parsedDate, err := time.Parse("1/2/2006 15:04:05", date)
-	if err != nil {
-		return "error"
-	}
+	parsedDate, _ := time.Parse("1/2/2006 15:04:05", date)
 	return "You have an appointment on " + parsedDate.Format("Monday, January 2, 2006, at 15:04.")
 }
 
