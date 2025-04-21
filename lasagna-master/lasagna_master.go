25ab2b891065d51a73f0546edb6e28da58974cf5
Руслан Ускембаев <ruslanuskembaev@MacBook-Pro-Ruslan.local>
2025-04-22 00:29:48 +0700

    11/141 done

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,18 +1,39 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTime int) int {
+	if prepTime == 0 {
+		prepTime = 2
+	}
+	return len(layers) * prepTime
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	var noddlesQtts int
+	var sauceQtts float64
+	for i := 0; i < len(layers); i++ {
+		if layers[i] == "noodles" {
+			noddlesQtts += 50
+		}
+		if layers[i] == "sauce" {
+			sauceQtts += 0.2
+		}
+	}
+	return noddlesQtts, sauceQtts
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList, myList []string) {
+	secretIngredient := friendsList[len(friendsList)-1]
+	myList[len(myList)-1] = secretIngredient
+}
 
 // TODO: define the 'ScaleRecipe()' function
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	scaledQuantities := make([]float64, len(quantities))
+	for i := 0; i < len(quantities); i++ {
+		scaledQuantities[i] = quantities[i] / 2 * float64(portions)
+	}
+	return scaledQuantities
+}
