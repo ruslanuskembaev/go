fca094cf563da72d98cf845813557c780c896228
Руслан Ускембаев <ruslanuskembaev@MacBook-Pro-Ruslan.local>
2025-04-22 02:52:05 +0700

    14/141 done

--- bird-watcher/bird_watcher.go
+++ bird-watcher/bird_watcher.go
@@ -3,17 +3,31 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	var result int
+	for i := 0; i < len(birdsPerDay); i++ {
+		result += birdsPerDay[i]
+	}
+	return result
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	var result int
+	for i := 0 + (week-1)*7; i < (week-1)*7+7; i++ {
+		result += birdsPerDay[i]
+
+	}
+	return result
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
+
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	bpd := birdsPerDay
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		bpd[i] = birdsPerDay[i] + 1
+	}
+	return bpd
 }
