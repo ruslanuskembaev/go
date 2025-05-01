36dcef16f43efbde06f5fbcdb74a09bf18fc03fd
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2025-05-02 00:17:35 +0700

    17/141 done

--- sorting-room/sorting_room.go
+++ sorting-room/sorting_room.go
@@ -1,8 +1,14 @@
 package sorting
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // DescribeNumber should return a string describing the number.
 func DescribeNumber(f float64) string {
-	panic("Please implement DescribeNumber")
+	return fmt.Sprintf("This is the number %.1f", f)
+	// return fmt.Sprintf("This is the number %s", strconv.FormatFloat(f, 'f', 1, 64)) ????????
 }
 
 type NumberBox interface {
@@ -11,7 +17,8 @@ type NumberBox interface {
 
 // DescribeNumberBox should return a string describing the NumberBox.
 func DescribeNumberBox(nb NumberBox) string {
-	panic("Please implement DescribeNumberBox")
+	// return fmt.Sprintf("This is a box containing the number %s.0", (strconv.Itoa(nb.Number())))   ???????
+	return fmt.Sprintf("This is a box containing the number %.1f", float64(nb.Number()))
 }
 
 type FancyNumber struct {
@@ -29,15 +36,33 @@ type FancyNumberBox interface {
 // ExtractFancyNumber should return the integer value for a FancyNumber
 // and 0 if any other FancyNumberBox is supplied.
 func ExtractFancyNumber(fnb FancyNumberBox) int {
-	panic("Please implement ExtractFancyNumber")
+	if fn, ok := fnb.(FancyNumber); ok {
+		numValue, err := strconv.Atoi(fn.Value())
+		if err == nil {
+			return numValue
+		}
+	}
+	return 0
 }
 
 // DescribeFancyNumberBox should return a string describing the FancyNumberBox.
 func DescribeFancyNumberBox(fnb FancyNumberBox) string {
-	panic("Please implement DescribeFancyNumberBox")
+	num := ExtractFancyNumber(fnb)
+	return fmt.Sprintf("This is a fancy box containing the number %.1f", float64(num))
 }
 
 // DescribeAnything should return a string describing whatever it contains.
 func DescribeAnything(i interface{}) string {
-	panic("Please implement DescribeAnything")
+	switch v := i.(type) {
+	case int:
+		return DescribeNumber(float64(v))
+	case float64:
+		return DescribeNumber(v)
+	case NumberBox:
+		return DescribeNumberBox(v)
+	case FancyNumberBox:
+		return DescribeFancyNumberBox(v)
+	default:
+		return "Return to sender"
+	}
 }
