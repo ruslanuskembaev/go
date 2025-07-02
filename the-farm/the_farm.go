5f8ef8750de7d90fae9b169c75e164e4726ef309
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2025-07-02 07:42:32 +0700

    20/141 done

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,10 +1,54 @@
 package thefarm
 
+import (
+	"errors"
+	"fmt"
+)
+
 // TODO: define the 'DivideFood' function
+func DivideFood(fc FodderCalculator, numberOfCows int) (float64, error) {
+	fooderAmount, err := fc.FodderAmount(numberOfCows)
+	if err != nil {
+		return 0, err
+	}
+	fatteningFactor, err := fc.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
+
+	return (fooderAmount * fatteningFactor) / float64(numberOfCows), err
+}
 
 // TODO: define the 'ValidateInputAndDivideFood' function
+func ValidateInputAndDivideFood(fc FodderCalculator, numberOfCows int) (float64, error) {
+	if numberOfCows <= 0 {
+		var errInvalidNumverOfCows = errors.New("invalid number of cows")
+		return 0, errInvalidNumverOfCows
+	}
+	fooderAmount, err := fc.FodderAmount(numberOfCows)
+	if err != nil {
+		return 0, err
+	}
+	fatteningFactor, err := fc.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
+
+	return (fooderAmount * fatteningFactor) / float64(numberOfCows), nil
+}
 
 // TODO: define the 'ValidateNumberOfCows' function
+func ValidateNumberOfCows(numOfCows int) error {
+	if numOfCows < 0 {
+		errInvalidNumberOfCows := fmt.Errorf("%d cows are invalid: there are no negative cows", numOfCows)
+		return errInvalidNumberOfCows
+	}
+	if numOfCows == 0 {
+		errInvalidNumberOfCows := fmt.Errorf("%d cows are invalid: no cows don't need food", numOfCows)
+		return errInvalidNumberOfCows
+	}
+	return nil
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
