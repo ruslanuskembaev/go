b8dcc8cf855c1374d3571f52aa7a7f6c2373ed90
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2025-07-09 03:02:07 +0500

    21/141 done

--- census/census.go
+++ census/census.go
@@ -10,20 +10,28 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return (&Resident{name, age, address})
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return (r.Name != "" && r.Address["street"] != "")
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	panic("Please implement Delete.")
+	r.Name = ""
+	r.Address = nil
+	r.Age = 0
 }
 
 // Count counts all residents that have provided the required information.
 func Count(residents []*Resident) int {
-	panic("Please implement Count.")
+	count := 0
+	for r := range residents {
+		if residents[r].HasRequiredInfo() {
+			count += 1
+		}
+	}
+	return count
 }
