0a318da478eb20838290ec620a9aa85dc03c6e75
Руслан Ускембаев <ruslanuskembaev02@gmail.com>
2025-07-10 21:59:33 +0500

    22/141 done

--- welcome-to-tech-palace/welcome_to_tech_palace.go
+++ welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,37 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	if customer == "" {
+		return "Welcome to the Tech Palace!"
+	}
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	if numStarsPerLine <= 0 {
+		return welcomeMsg
+	}
+	border := ""
+	for i := 0; i < numStarsPerLine; i++ {
+		border += "*"
+	}
+	return border + "\n" + welcomeMsg + "\n" + border
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	if oldMsg == "" {
+		return ""
+	}
+	cleanedMsg := ""
+	for _, char := range oldMsg {
+		if char != '*' && char != '\n' {
+			cleanedMsg += string(char)
+		}
+	}
+	return strings.TrimSpace(cleanedMsg)
 }
